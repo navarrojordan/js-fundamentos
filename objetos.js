6bde068c2455f95f5c9b65a80054ca965cabe0df
Jordan Navarro <jordan.navarro1@live.com>
2020-03-03 22:16:15 -0300

    Criando meu primeiro commit com o javascript

---
+++ objetos.js
@@ -0,0 +1,81 @@
+
+//declarando objetos
+let person = {};
+
+
+/*
+declarando propriedades do meu objeto
+*/
+
+let person = {
+  name: 'Carlos Da Silva',
+  age: 39,
+  sex: 'male',
+  single: false
+};
+
+let cars ={
+
+  company: 'Fiat',
+  model: '147',
+  sold: false,
+  color: 'Blue',
+  doors:3
+};
+
+
+let cars = 2{
+
+  company: 'Volks',
+  model: 'fusca',
+  sold: true,
+  color: 'Black',
+  doors:3
+};
+
+
+function sayname(name) {
+
+  return `Olá, ${name}`;
+
+}
+sayname ('Carlos');
+
+function sum(a,b){
+  return a+b;
+}
+
+sum(0,9);
+
+  var money = true;
+
+  function sayMoney(){
+    money = 'abacaxi';
+
+  if (moeda){
+    return `Seu valor é  ${moeda}`
+  }
+  return `Não temos valor aqui!`;
+  }
+
+
+  var frutas = [ 'abacaxi', 'pera', 'tomate', 'abacate'  ];
+
+  //funçao anonima
+  var soma = function(a+b){
+return a+b;
+
+  }
+
+  var soma = =>(a+b){
+    return a+b;
+    
+   }
+
+let animal = 'cachorro';
+typeof animal;
+
+let verdadeiro = true;
+typeof verdadeiro;
+
+git config --global user.email "jordan.navarro1@live.com"
